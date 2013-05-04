aeb0719e3061760148e5310b4f54d543f530f9bb
Henry Lee <henrylee@boohee.com>
2013-05-04 15:47:30 +0800

    [Change] remove extra models

--- app/models/boohee/city.rb
+++
@@ -1,5 +0,0 @@
-# -*- encoding : utf-8 -*-
-
-class Boohee::City < Boohee::BooheeModel
-  has_many  :province, :class_name => 'Boohee::Province'
-end
--- app/models/boohee/province.rb
+++
@@ -1,5 +0,0 @@
-# -*- encoding : utf-8 -*-
-
-class Boohee::Province < Boohee::BooheeModel
-  has_many  :cities, :class_name => 'Boohee::City'
-end
